a93af9afa50deded996a0c62789a28425ee91c61
Cássio Kenji <cassio.oshiro@walmart.com>
2016-11-23 22:48:28 -0200

    add driver configuration block: chrome driver

--- features/support/env.rb
+++ features/support/env.rb
@@ -5,3 +5,12 @@ Pry.config.color = true
 SitePrism.configure do |config|
   config.use_implicit_waits = true
 end
+
+Capybara.register_driver :chrome do |app|
+    caps = Selenium::WebDriver::Remote::Capabilities.chrome('chromeOptions' => { 'args' => ['--start-maximized'] })
+    Capybara::Selenium::Driver.new(app, browser: :chrome, desired_capabilities: caps)
+  end
+
+Capybara.default_driver = $capybara_driver
+Capybara.default_max_wait_time = 15
+# Capybara.page.driver.browser.manage.window.maximize
